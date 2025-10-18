6a2aca7bdebad9dbdecfc3172bb8c6ac91247ac4
Pablo Ripoll <pabloripoll.it@gmail.com>
2025-10-18 02:21:20 +0200

    feat: add unique to column name to avoid duplications

--- database/migrations/2025_10_01_193403_create_geo_continents_table.php
+++ database/migrations/2025_10_01_193403_create_geo_continents_table.php
@@ -13,7 +13,7 @@ return new class extends Migration
     {
         Schema::create('geo_continents', function (Blueprint $table) {
             $table->id();
-            $table->string('name', 64);
+            $table->string('name', 64)->unique();
             $table->timestamps();
         });
     }
