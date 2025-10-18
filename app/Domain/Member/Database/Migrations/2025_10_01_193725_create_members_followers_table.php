a26639ea0daf162527c75dde6d916e9865c92be4
Pablo Ripoll <pabloripoll.it@gmail.com>
2025-10-18 02:24:59 +0200

    feat: table refactored by the corresponding design

--- app/Domain/Member/Database/Migrations/2025_10_01_193725_create_members_followers_table.php
+++ app/Domain/Member/Database/Migrations/2025_10_01_193725_create_members_followers_table.php
@@ -15,15 +15,8 @@ return new class extends Migration
         Schema::create('members_followers', function (Blueprint $table) {
             $table->id();
             $table->foreignId('user_id')->constrained((new User)->getTable());
-            $table->boolean('seen_by_user')->default(false);
-            $table->timestamp('seen_by_user_at')->nullable()->index();
+            $table->foreignId('follower_user_id')->constrained((new User)->getTable());
             $table->timestamps();
-            $table->index('created_at');
-            $table->boolean('is_post_visit')->default(false);
-            $table->boolean('is_post_vote')->default(false);
-            $table->boolean('is_new_member_post')->default(false);
-            $table->foreignId('last_member_id')->constrained((new User)->getTable());
-            $table->string('message', 256);
         });
     }
 
