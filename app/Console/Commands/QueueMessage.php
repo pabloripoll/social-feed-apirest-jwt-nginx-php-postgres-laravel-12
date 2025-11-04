a8a820376cef6470039e521557bcbd03a9d833e1
Pablo Ripoll <pabloripoll.it@gmail.com>
2025-11-04 12:04:47 +0100

    feat: rabbitmq installation and configuration

---
+++ app/Console/Commands/QueueMessage.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use PhpAmqpLib\Message\AMQPMessage;
+use PhpAmqpLib\Connection\AMQPStreamConnection;
+
+class QueueMessage extends Command
+{
+    protected $signature = 'rabbit:produce {message}';
+    protected $description = 'Produce a message to RabbitMQ';
+
+    public function handle()
+    {
+        $queue = env('RABBITMQ_EXCHANGE_QUEUE');
+
+        $msgBody = $this->argument('message');
+        $connection = new AMQPStreamConnection(
+            config('queue.connections.rabbitmq.host'),
+            config('queue.connections.rabbitmq.port'),
+            config('queue.connections.rabbitmq.login'),
+            config('queue.connections.rabbitmq.password'),
+            config('queue.connections.rabbitmq.vhost')
+        );
+        $channel = $connection->channel();
+        $channel->queue_declare($queue, false, true, false, false);
+
+        $msg = new AMQPMessage($msgBody, ['delivery_mode' => AMQPMessage::DELIVERY_MODE_PERSISTENT]);
+        $channel->basic_publish($msg, '', $queue);
+
+        $channel->close();
+        $connection->close();
+
+        $this->info('Message published');
+    }
+}
