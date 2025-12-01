a8c3ed282518c6c7943856dbfc23ba2605b73784
Pablo Ripoll <pabloripoll.it@gmail.com>
2025-12-02 00:02:34 +0100

    refactor: member feed routes endpoint contract

--- app/Domain/Feed/Routes/routes.php
+++ app/Domain/Feed/Routes/routes.php
@@ -25,4 +25,15 @@ Route::prefix('/api/v1')->name('api-v1.')->group(function () {
             Route::delete('/posts/{post_id}/votes/down', [FeedVoteController::class, 'deleteVoteDown'])->name('post-vote-down-delete');
         });
     });
+
+    Route::prefix('/account/feed')->middleware(['jwt', 'member'])->name('feed.')->group(function () {
+        Route::get('/posts', [FeedPostController::class, 'listPosts'])->name('posts-listing');
+        Route::post('/posts', [FeedPostController::class, 'createPost'])->name('post-create');
+        Route::get('/posts/{post_id}', [FeedPostController::class, 'getPost'])->name('post-read');
+        Route::put('/posts/{post_id}', [FeedPostController::class, 'activatePost'])->name('post-activate');
+        Route::patch('/posts/{post_id}', [FeedPostController::class, 'updatePost'])->name('post-patch');
+        Route::delete('/posts/{post_id}', [FeedPostController::class, 'deletePost'])->name('post-delete');
+        Route::post('/posts/{post_id}/media', [FeedPostController::class, 'uploadPostMedia'])->name('post-media-upload');
+        Route::delete('/posts/{post_id}/media', [FeedPostController::class, 'deletePostMedia'])->name('post-media-delete');
+    });
 });
--- app/Domain/Member/Routes/routes.php
+++ app/Domain/Member/Routes/routes.php
@@ -28,15 +28,6 @@ Route::prefix('/api/v1')->name('api-v1.')->group(function () {
         Route::post('/profile/avatar', [MemberAccountController::class, 'uploadAvatar'])->name('upload-avatar');
         Route::delete('/profile/avatar', [MemberAccountController::class, 'deleteAvatar'])->name('delete-avatar');
 
-        Route::get('/posts', [FeedPostController::class, 'listPosts'])->name('posts-listing');
-        Route::post('/posts', [FeedPostController::class, 'createPost'])->name('post-create');
-        Route::get('/posts/{post_id}', [FeedPostController::class, 'getPost'])->name('post-read');
-        Route::put('/posts/{post_id}', [FeedPostController::class, 'activatePost'])->name('post-activate');
-        Route::patch('/posts/{post_id}', [FeedPostController::class, 'updatePost'])->name('post-patch');
-        Route::delete('/posts/{post_id}', [FeedPostController::class, 'deletePost'])->name('post-delete');
-        Route::post('/posts/{post_id}/media', [FeedPostController::class, 'uploadPostMedia'])->name('post-media-upload');
-        Route::delete('/posts/{post_id}/media', [FeedPostController::class, 'deletePostMedia'])->name('post-media-delete');
-
         Route::get('/notifications', [MemberAccountController::class, 'listNotifications'])->name('list-notifications');
         Route::put('/notifications/{notification_id}/read', [MemberAccountController::class, 'setNotificationRead'])->name('set-notification-read');
     });
