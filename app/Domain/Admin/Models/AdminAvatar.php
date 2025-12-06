91ea8a48de641a3a1e339540d9d7a5323fb2e622
Pablo Ripoll <pabloripoll.it@gmail.com>
2025-12-06 17:14:40 +0100

    feat: admin avatar scripts added

---
+++ app/Domain/Admin/Models/AdminAvatar.php
@@ -0,0 +1,73 @@
+<?php
+
+namespace App\Domain\Admin\Models;
+
+use App\Domain\User\Models\User;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use App\Domain\Admin\Database\Factories\AdminAvatarFactory;
+
+class AdminAvatar extends Model
+{
+    /** @use HasFactory<\App\Domain\Admin\Database\Factories\AdminAvatar> */
+    //use HasFactory;
+
+    /**
+     * @var string
+     */
+    protected $table = 'admins_avatars';
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var list<string>
+     */
+    protected $fillable = [
+        'user_id',
+        'is_selected',
+        'position',
+        'type',
+        'extension',
+        'path',
+        'name',
+        'title',
+        'url',
+    ];
+
+    /**
+     * The attributes that should be hidden for serialization.
+     *
+     * @var list<string>
+     */
+    protected $hidden = [];
+
+    /**
+     * Get the attributes that should be cast.
+     *
+     * @return array<string, string>
+     */
+    protected function casts(): array
+    {
+        return [
+            'created_at' => 'datetime',
+            'updated_at' => 'datetime',
+        ];
+    }
+
+    /**
+     * Model factory when is outside ./database/factories
+     */
+    public static function newFactory()
+    {
+        return AdminAvatarFactory::new();
+    }
+
+    /**
+     * Relations
+     */
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+}
