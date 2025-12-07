211a1bfaff64e285479e7c109a005d8191cc2a68
Pablo Ripoll <pabloripoll.it@gmail.com>
2025-12-07 01:00:21 +0100

    feat: member user account unused routes removed

--- app/Domain/Member/Routes/routes.php
+++ app/Domain/Member/Routes/routes.php
@@ -12,22 +12,16 @@ Route::prefix('/api/v1')->name('api-v1.')->group(function () {
         Route::post('/login', [MemberAuthController::class, 'login'])->name('login');
 
         Route::middleware(['jwt', 'member'])->group(function () {
-
             Route::get('/settings', [MemberAccountController::class, 'readSettings'])->name('read-settings');
             Route::get('/access-logs', [MemberAccountController::class, 'listAccessLogs'])->name('read-access-logs');
-
             Route::get('/profile', [MemberProfileController::class, 'readProfile'])->name('read-profile');
-
-            Route::put('/profile/nickname', [MemberProfileController::class, 'updateProfileNickname'])->name('update-profile-nickname');
-
-            Route::get('/profile/avatars', [MemberAccountController::class, 'listAvatar'])->name('list-avatars');
-            Route::post('/profile/avatars', [MemberAccountController::class, 'uploadAvatar'])->name('upload-avatar');
-            Route::delete('/profile/avatars/{avatar_id}/select', [MemberAccountController::class, 'selectAvatarById'])->name('select-avatar');
-            Route::delete('/profile/avatars/{avatar_id}', [MemberAccountController::class, 'deleteAvatarById'])->name('delete-avatar');
+            Route::get('/avatars', [MemberAccountController::class, 'listAvatar'])->name('list-avatars');
+            Route::post('/avatars', [MemberAccountController::class, 'uploadAvatar'])->name('upload-avatar');
+            Route::delete('/avatars/{avatar_id}/select', [MemberAccountController::class, 'selectAvatarById'])->name('select-avatar');
+            Route::delete('/avatars/{avatar_id}', [MemberAccountController::class, 'deleteAvatarById'])->name('delete-avatar');
 
             Route::get('/notifications', [MemberAccountController::class, 'listNotifications'])->name('list-notifications');
             Route::put('/notifications/{notification_id}/read', [MemberAccountController::class, 'setNotificationRead'])->name('set-notification-read');
-
         });
     });
 
@@ -36,5 +30,4 @@ Route::prefix('/api/v1')->name('api-v1.')->group(function () {
         Route::get('/{member_uid}/profile', [MemberProfileController::class, 'readMemberProfile'])->name('read-profile');
         Route::get('/{member_uid}/posts', [MemberProfileController::class, 'listPosts'])->name('list-posts');
     });
-
 });
